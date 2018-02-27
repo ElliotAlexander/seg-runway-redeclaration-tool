c5fbd366d601775b57fbaced0afd8a92d24c7fcc
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-02-27 22:53:12 +0000

    RunwayParameters constructor and sketched RunwayFactory.getRunway

--- src/main/java/RunwayRedeclarationTool/Models/RunwayFactory.java
+++ src/main/java/RunwayRedeclarationTool/Models/RunwayFactory.java
@@ -4,7 +4,12 @@ package RunwayRedeclarationTool.Models;
 public class RunwayFactory {
     public static final RunwayFactory instance = new RunwayFactory();
 
-    private RunwayFactory() {
-
+    public static RunwayFactory getInstance () {
+        return instance;
     }
+
+//    public Runway getRunway(String designator, int TORA, int TODA, int ASDA, int LDA, int displacedThreshold) {
+//        // DO WE NEED TO PERFORM ANY CHECKS ON INPUT?
+//        RunwayParameters params = new RunwayParameters(TORA, TODA, ASDA, LDA, displacedThreshold);
+//    }
 }
--- src/main/java/RunwayRedeclarationTool/Models/RunwayParameters.java
+++ src/main/java/RunwayRedeclarationTool/Models/RunwayParameters.java
@@ -2,4 +2,13 @@ package RunwayRedeclarationTool.Models;
 
 // Holds TORA, TODA, ASDA, LDA and whatever else needed
 public class RunwayParameters {
+    protected int TORA, TODA, ASDA, LDA, displacedThreshold;
+
+    public RunwayParameters (int TORA, int TODA, int ASDA, int LDA, int displacedThreshold) {
+        this.TORA = TORA;
+        this.TODA = TODA;
+        this.ASDA = ASDA;
+        this.LDA = LDA;
+        this.displacedThreshold = displacedThreshold;
+    }
 }
