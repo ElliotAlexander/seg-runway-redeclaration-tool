52aca3b0350815854f7f63bb525c185e93800402
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-03-03 16:42:33 +0000

    A bit of code separation in preparation of calculation breakdown.

--- src/main/java/RunwayRedeclarationTool/Models/Calculator.java
+++ src/main/java/RunwayRedeclarationTool/Models/Calculator.java
@@ -28,49 +28,58 @@ public class Calculator {
             throw new NoRedeclarationNeededException("Obstacle is outside visual strip.");
         }
 
-        // To which threshold is the object closer?
+        decideSideOfRunway(o, r);
+    }
+
+    public void decideSideOfRunway (Obstacle o, Runway r) {
         if (o.getDistLeftTSH() < o.getDistRightTSH()) {
-            r.leftRunway.setRecalcParams(takeoffAwayLandOver(o, leftParams, o.getDistLeftTSH()));
-            r.rightRunway.setRecalcParams(takeoffTowardsLandTowards(o, rightParams, o.getDistRightTSH()));
+            takeoffAwayLandOver(o, r.leftRunway, o.getDistLeftTSH());
+            takeoffTowardsLandTowards(o, r.rightRunway, o.getDistRightTSH());
         } else {
-            r.leftRunway.setRecalcParams(takeoffTowardsLandTowards(o, leftParams, o.getDistLeftTSH()));
-            r.rightRunway.setRecalcParams(takeoffAwayLandOver(o, rightParams, o.getDistRightTSH()));
+            takeoffAwayLandOver(o, r.rightRunway, o.getDistRightTSH());
+            takeoffTowardsLandTowards(o, r.leftRunway, o.getDistLeftTSH());
         }
     }
 
-    private RunwayParameters takeoffAwayLandOver (Obstacle o, RunwayParameters params, int distFromTSH) {
+    // Requires the smaller distance from threshold
+    private void takeoffAwayLandOver (Obstacle o, VirtualRunway vRunway, int distFromTSH) {
+        // Setting up all needed values
+        RunwayParameters params = vRunway.getOrigParams();
         int stopway = params.ASDA - params.TORA;
         int clearway = params.TODA - params.TORA;
         int displacedTSH = params.TORA - params.LDA;
 
-        // take off
+        // Take off distances
         int rTORA = params.TORA - distFromTSH - displacedTSH - visual_strip_end - RESA;
         int rASDA = rTORA + stopway;
         int rTODA = rTORA + clearway;
 
-        // land
+        // Landing distances
         int slopeCalc = o.getHeight() * 50;
         if (slopeCalc < RESA) {
             slopeCalc = RESA;  // minimum distance from obstacle: RESA + strip end
         }
         int rLDA = params.LDA - distFromTSH - slopeCalc - visual_strip_end;
 
-        return new RunwayParameters(rTORA, rTODA, rASDA, rLDA);
+        vRunway.setRecalcParams(new RunwayParameters(rTORA, rTODA, rASDA, rLDA));
     }
 
-    private RunwayParameters takeoffTowardsLandTowards(Obstacle o, RunwayParameters params, int distFromTSH) {
+    // Requires the greater distance from threshold
+    private void takeoffTowardsLandTowards(Obstacle o, VirtualRunway vRunway, int distFromTSH) {
+        // Setting up all needed values
+        RunwayParameters params = vRunway.getOrigParams();
         int displacedTSH = params.TORA - params.LDA;
 
-        // take off
+        // Take off distances
         int slopeCalc = o.getHeight() * 50;
         if (slopeCalc < RESA) {
             slopeCalc = RESA;
         }
         int rTORA = distFromTSH + displacedTSH - slopeCalc - visual_strip_end;
 
-        // land
+        // Landing distances
         int rLDA = distFromTSH - RESA - visual_strip_end;
 
-        return new RunwayParameters(rTORA, rTORA, rTORA, rLDA);
+        vRunway.setRecalcParams(new RunwayParameters(rTORA, rTORA, rTORA, rLDA));
     }
 }
