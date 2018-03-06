5069cfcdb36e1d61e8c59adf00d6c903c142f674
Elliot Alexander <epurvis97@gmail.com>
2018-03-06 05:33:47 +0000

    Moved logs into dedicated directory

---
+++ src/main/java/RunwayRedeclarationTool/Logger/FileLogger.java
@@ -0,0 +1,47 @@
+package RunwayRedeclarationTool.Logger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileLogger {
+
+    public static final String LOG_FILE = "log.txt";
+
+    private FileOutputStream os = null;
+
+
+    // Protected constructor, instantiated by logger.
+    protected FileLogger(){
+        try {
+
+
+            // will have no effect if dir already exists
+            File log_dir = new File("logs/");
+            log_dir.mkdir();
+
+
+            File f = new File(log_dir.getAbsolutePath() + "/" + LOG_FILE);
+            f.createNewFile();
+            os = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            Logger.Log(Logger.Level.ERROR, "Cannot open log file for writing!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void Log(String s){
+        try {
+            os.write(s.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/RunwayRedeclarationTool/Logger/Logger.java
@@ -0,0 +1,23 @@
+package RunwayRedeclarationTool.Logger;
+
+public class Logger {
+
+
+
+    public enum Level{INFO, WARNING, ERROR, MESSAGE}
+    private static final FileLogger fl = new FileLogger();
+
+
+    public static void Log(Level l, String message){
+        System.out.println("[SERVER " + l + "] " + message);
+        fl.Log("[" + l + "] " +  message);
+    }
+
+
+    // Defaults to info
+    public static void Log(String message){
+        System.out.println("[SERVER INFO] " + message);
+        fl.Log("[" + Level.INFO + "] " +  message);
+    }
+
+}
