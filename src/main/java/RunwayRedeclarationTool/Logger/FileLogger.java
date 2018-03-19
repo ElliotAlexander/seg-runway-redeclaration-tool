4dc189ac0d194aac266021da73b22475cc43f690
Elliot Alexander <epurvis97@gmail.com>
2018-03-19 05:35:05 +0000

    Tidying up

--- src/main/java/RunwayRedeclarationTool/Logger/FileLogger.java
+++ src/main/java/RunwayRedeclarationTool/Logger/FileLogger.java
@@ -7,21 +7,21 @@ import java.io.IOException;
 
 public class FileLogger {
 
-    public static final String LOG_FILE = "log.txt";
 
     private FileOutputStream os = null;
 
 
     // Protected constructor, instantiated by logger.
-    protected FileLogger(){
+    protected FileLogger(String logdir, String logstr){
         try {
             // will have no effect if dir already exists
-            File log_dir = new File("logs/");
+            File log_dir = new File(logdir);
             log_dir.mkdir();
 
-            File f = new File(log_dir.getAbsolutePath() + "/" + LOG_FILE);
+            File f = new File(log_dir.getAbsolutePath() + "/" + logstr);
             f.createNewFile();
             os = new FileOutputStream(f);
+            os.write("[WARNING] Logs may have been missed before this point. Run the Jar from the command line for full output prior to File Logger starting up.".getBytes());
         } catch (FileNotFoundException e) {
             Logger.Log(Logger.Level.ERROR, "Cannot open log file for writing!");
             e.printStackTrace();
--- src/main/java/RunwayRedeclarationTool/Logger/Logger.java
+++ src/main/java/RunwayRedeclarationTool/Logger/Logger.java
@@ -1,19 +1,38 @@
 package RunwayRedeclarationTool.Logger;
 
+import RunwayRedeclarationTool.Config.Configuration;
+import RunwayRedeclarationTool.Exceptions.ConfigurationKeyNotFound;
+
 public class Logger {
 
     public enum Level{INFO, WARNING, ERROR}
-    private static final FileLogger fl = new FileLogger();
+    private static FileLogger fl = null;
+
+    // TODO maybe tidy this class a lot
+
+    public Logger(Configuration config){
+        try {
+            fl = new FileLogger(config.getConfigurationValue("LogDirectory"), config.getConfigurationValue("LogFile"));
+        } catch (ConfigurationKeyNotFound configurationKeyNotFound) {
+            configurationKeyNotFound.printStackTrace();
+        }
+    }
 
     public static void Log(Level l, String message){
         System.out.println("[" + l + "] " + message);
-        fl.Log("[" + l + "] \n" +  message + "\n");
+
+        // This is honestly pretty sketch but should be safe.
+        if(fl != null){
+            fl.Log("[" + l + "] \n" +  message + "\n");
+        }
     }
 
 
     // Defaults to info
     public static void Log(String message){
         System.out.println("[INFO] " + message);
-        fl.Log("[" + Level.INFO + "] " +  message + "\n");
+        if(fl != null){
+            fl.Log("[" + Level.INFO + "] " +  message + "\n");
+        }
     }
 }
