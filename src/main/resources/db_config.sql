2d458166d0f977b389de641f99cc340c6bec9a3a
Elliot Alexander <epurvis97@gmail.com>
2018-03-31 09:37:10 +0100

    refactor (SQL): Removed length from Obstacle table schema.

--- src/main/resources/db_config.sql
+++ src/main/resources/db_config.sql
@@ -18,7 +18,6 @@ CREATE TABLE IF NOT EXISTS `runway` (
 CREATE TABLE IF NOT EXISTS `obstacle`(
 `obstacle_id` INT NOT NULL,
 `obstacle_name` VARCHAR(45) NULL,
-`length` INT NULL,
 `height` INT NULL,
 PRIMARY KEY(`obstacle_id`));
 
