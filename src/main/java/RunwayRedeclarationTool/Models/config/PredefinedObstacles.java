c546a8bfc4fe9b92ecb4185b9776586abeb4ca33
Elliot Alexander <epurvis97@gmail.com>
2018-04-01 09:56:30 +0100

    feat (Predefined Obstacles): Adding loading of predefined obstacles from a text file included in resources.

--- src/main/java/RunwayRedeclarationTool/Models/config/PredefinedObstacles.java
+++ src/main/java/RunwayRedeclarationTool/Models/config/PredefinedObstacles.java
@@ -1,20 +1,80 @@
 package RunwayRedeclarationTool.Models.config;
 
 import RunwayRedeclarationTool.Exceptions.ConfigurationKeyNotFound;
+import RunwayRedeclarationTool.Logger.Logger;
+import RunwayRedeclarationTool.Models.Obstacle;
 import RunwayRedeclarationTool.Models.db.DB_controller;
 import RunwayRedeclarationTool.Models.xml.XML_Parser;
+import jdk.nashorn.internal.runtime.arrays.ArrayIndex;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PredefinedObstacles {
 
 
-    private final String Default_Obstacles_File = "default_obstacles.xml";
-    private final String Default_Airports_File = "default_airports.xml";
+    // This class could be a lot quicker if we hard coded in obstacles
+    // My personal preference is to load from a file?
+    // Although this could negatively impact startup time pretty considerably
+    // TODO - Consider this?
 
+    private final String default_obstacles_file = "default_obstacles.txt";
 
     public void addDefaults(DB_controller dbc, Configuration configuration){
 
+        try {
+            if(configuration.getConfigurationValue("LoadpredefinedAirportsfromfile").equalsIgnoreCase("yes")){
+                obstacle_loop:
+                for(Obstacle o : load_obstacles_file()){
+                    for(Obstacle o2 : dbc.get_obstacles()){
+                        if(o2.getName().equalsIgnoreCase(o.getName())){
+                            Logger.Log("Skipping already existing obstacle \'" + o.getName() + "\'.");
+                            continue obstacle_loop;
+                        }
+                    }
+                    dbc.add_obstacle(o);
+                }
+            }
+        } catch (ConfigurationKeyNotFound configurationKeyNotFound) {
+            configurationKeyNotFound.printStackTrace();
+        }
+    }
+
+
+
+    private Obstacle[] load_obstacles_file(){
+        ArrayList<Obstacle> return_list = new ArrayList<Obstacle>();
+        //Get file from resources folder
+        InputStream is = DB_controller.class.getClassLoader().getResourceAsStream(default_obstacles_file);
+        try {
+            if(!(is.available() == 0)){
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        String[] values = line.split(":");
+                        String name = values[0];
+                        Integer height = Integer.parseInt(values[1]);
+                        return_list.add(new Obstacle(name, height));
+                    }
+                    return return_list.toArray(new Obstacle[return_list.size()]);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ArrayIndexOutOfBoundsException e ){
+                    Logger.Log(Logger.Level.ERROR, "Error Loading predefined obstacles file!\nArrayIndexOutOfBoundsException");
+                    Logger.Log(Logger.Level.ERROR, e.getStackTrace().toString());
+                } catch (NumberFormatException e){
+                    Logger.Log(Logger.Level.ERROR, "NumberFormatException when processing default_obstacles file.");
+                    Logger.Log(Logger.Level.ERROR, e.getStackTrace().toString());
+                }
+            } else {
+                Logger.Log(Logger.Level.ERROR, "Failed to load default obstacles file! [Filename=\'"+default_obstacles_file+"\'].");
+                return null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
