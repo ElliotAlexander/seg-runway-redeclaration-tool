1acbcfa60977cac5c1febdce3f588e1642cbbf66
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-04-28 22:39:27 +0100

    fix(TOCS/ALS slope): Add strip end to display

--- src/main/java/RunwayRedeclarationTool/View/SideOnView.java
+++ src/main/java/RunwayRedeclarationTool/View/SideOnView.java
@@ -2,7 +2,6 @@ package RunwayRedeclarationTool.View;
 
 import RunwayRedeclarationTool.Exceptions.AttributeNotAssignedException;
 import RunwayRedeclarationTool.Models.ObstaclePosition;
-import RunwayRedeclarationTool.Models.RunwayParameters;
 import RunwayRedeclarationTool.Models.VirtualRunway;
 import javafx.scene.paint.Color;
 
@@ -78,20 +77,21 @@ public class SideOnView extends RunwayView {
         gc.setGlobalAlpha(0.5);
 
         int oHeight = obstaclePosition.getObstacle().getHeight();
+        int slopeLength = slopecalc + 60;   // slope calculation + 60
 
         if (obstaclePosition.getDistLeftTSH() < obstaclePosition.getDistRightTSH()) {
             if (leftRunway) {
                 // __|=|__->______
                 int endObstacle = leftSpace + obstaclePosition.getDistLeftTSH() + oLength;
 
-                gc.fillPolygon(new double[]{scale_x(endObstacle), scale_x(endObstacle), scale_x(endObstacle + slopecalc)}, new double[]{scale_y(149 - oHeight), scale_y(149), scale_y(149)}, 3);
+                gc.fillPolygon(new double[]{scale_x(endObstacle), scale_x(endObstacle), scale_x(endObstacle + slopeLength)}, new double[]{scale_y(149 - oHeight), scale_y(149), scale_y(149)}, 3);
             }
         } else {
             if (!leftRunway) {
                 // ______<-__|=|__
                 int startObstacle = leftSpace + obstaclePosition.getDistLeftTSH();
 
-                gc.fillPolygon(new double[]{scale_x(startObstacle - slopecalc), scale_x(startObstacle), scale_x(startObstacle)}, new double[]{scale_y(149), scale_y(149), scale_y(149 - oHeight)}, 3);
+                gc.fillPolygon(new double[]{scale_x(startObstacle - slopeLength), scale_x(startObstacle), scale_x(startObstacle)}, new double[]{scale_y(149), scale_y(149), scale_y(149 - oHeight)}, 3);
             }
         }
 
