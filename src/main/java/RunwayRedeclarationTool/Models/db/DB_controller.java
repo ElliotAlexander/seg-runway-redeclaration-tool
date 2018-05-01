35ff00b4217be949542e16bdd5db7295470e5882
Elliot Alexander <epurvis97@gmail.com>
2018-05-01 22:12:49 +0100

    fix (DB): Fixed a bug where Remove Airport would be run on the database twice.

--- src/main/java/RunwayRedeclarationTool/Models/db/DB_controller.java
+++ src/main/java/RunwayRedeclarationTool/Models/db/DB_controller.java
@@ -211,11 +211,9 @@ public class DB_controller
 
     public void remove_Airport(Airport airport){
         String remove_airport = "DELETE FROM airport WHERE airport_id=\'" + airport.getAirport_id() + "\';";
-        String remove_runways = "DELETE FROM runway WHERE airport_id=\'" + airport.getAirport_id() + "\';";
         Logger.Log("Removing all Runways with Airport ID " + airport.getAirport_id());
         Logger.Log("Removing airport " + airport.getAirport_name());
         execute(false, remove_airport);
-        execute(false, remove_runways);
     }
 
     public Airport[] get_airports(){
